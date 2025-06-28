43442de3d66352fdb4bb1eb48517e8ce141f77ce
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2025-06-28 22:48:32 +0200

    list comparison

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <list>
 #include <stdexcept>
 #include <vector>
 #include <cassert>
@@ -9,6 +10,7 @@
 
 using std::cout;
 using std::endl;
+using std::list;
 using std::string;
 using std::vector;
 
@@ -43,7 +45,38 @@ static void subjectTest() {
 	stack<int> s(mstack);
 }
 
+static void listTest() {
+	list<int> mstack;
+	mstack.push_back(5);
+	assert(mstack.size() == 1);
+	assert(*mstack.begin() == 5);
+	mstack.push_back(17);
+	assert(mstack.size() == 2);
+	assert(*mstack.begin() == 5);
+	assert(mstack.back() == 17);
+	mstack.pop_back();
+	assert(mstack.size() == 1);
+	assert(*mstack.begin() == 5);
+	assert(mstack.back() == 5);
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	mstack.push_back(0);
+	assert(mstack.size() == 5);
+	list<int>::iterator it = mstack.begin();
+	list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	int exp[] = {5, 3, 5, 737, 0};
+	int i = 0;
+	while (it != ite) {
+		assert(*it == exp[i++]);
+		++it;
+	}
+	list<int> s(mstack);
+}
 int main() {
 	subjectTest();
+	listTest();
 	return 0;
 }
